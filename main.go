ab0802240cba2578b34bac31fb8d4da13dc46450
akashdeep931 <akashdeepsingh931@gmail.com>
2025-11-16 19:38:45 +0000

    setup db, schema and create user endoint

--- main.go
+++ main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/akashdeep931/rss/internal/db"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *db.Queries
+}
 
 func main() {
 	godotenv.Load()
-	portString := os.Getenv("PORT")
 
+	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")	
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")	
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+
+	if err != nil {
+		log.Fatal("An error has occurred when performing a db connection")
+	}
+
+	queries := db.New(conn)
+
+	apiCfg := apiConfig{
+		DB: queries,
 	}
 
 	router := chi.NewRouter()
@@ -30,8 +55,10 @@ func main() {
 	}))
 
 	v1Router := chi.NewRouter()
+
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/error", handlerError)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -41,7 +68,8 @@ func main() {
 	}
 
 	log.Printf("Server starting on port: %s", portString)
-	err := srv.ListenAndServe()
+
+	err = srv.ListenAndServe()
 
 	if err != nil {
 		log.Fatal(err)
---
+++ models.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"time"
+
+	"github.com/akashdeep931/rss/internal/db"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string `json:"name"`
+}
+
+func dbUserToUser(dbUser db.User) User {
+	return User{
+		ID: dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Name: dbUser.Name,
+	}
+}
